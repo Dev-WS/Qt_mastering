a3dffc0f78fbc5d336a170e75e29540391b65409
Wojciech Strzalinski <wojciech.strzalinski@apsenergia.pl>
2021-06-15 11:22:57 +0200

    Added QDialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QInputDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow), mTasks()
 {
     ui->setupUi(this);
 
@@ -20,6 +21,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::addTask()
 {
-    qDebug() << "User clicked on the button!";
+    bool ok;
+
+    QString name = QInputDialog::getText(this, tr("Add task"), tr("Task name"), QLineEdit::Normal, tr("Untitled task"), &ok);
+
+    if (ok && !name.isEmpty()){
+
+        qDebug() << "Adding new task";
+        Task* task = new Task("Untitled task");
+        mTasks.append(task);
+        ui->tasksLayout->addWidget(task);
+
+    }
 
 }
